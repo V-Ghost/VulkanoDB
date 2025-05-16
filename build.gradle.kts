98ba0f1f819e6ec787ee82fa659a29609ac7fdab
HellowWorld <vukaniabryan@gmail.com>
2025-05-16 17:01:00 +0100

    chore: build.gradle added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "org.vulkano"
@@ -11,21 +12,56 @@ repositories {
 }
 
 dependencies {
+    // Testing
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    // Compression
     implementation("org.xerial.snappy:snappy-java:1.1.10.7")
-    implementation ("io.grpc:grpc-netty-shaded:1.63.0")
-    implementation ("io.grpc:grpc-protobuf:1.63.0")
-    implementation ("io.grpc:grpc-stub:1.63.0")
-    implementation ("com.google.protobuf:protobuf-java:3.25.3")
+
+    // gRPC and Protobuf
+    implementation("io.grpc:grpc-netty-shaded:1.63.0")
+    implementation("io.grpc:grpc-protobuf:1.63.0")
+    implementation("io.grpc:grpc-stub:1.63.0")
+    implementation("com.google.protobuf:protobuf-java:3.25.3")
 }
 
 application {
-    // Replace with your actual fully-qualified main class name
     mainClass.set("org.vulkano.Main")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.63.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/proto")
+        }
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/java",
+                "build/generated/source/proto/main/grpc"
+            )
+        }
+    }
 }
